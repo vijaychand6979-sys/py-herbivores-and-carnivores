a76ee01defa959e0d5c9df83d1a77ce7530dc731
Vijay Chand <vijaychand6979@googlemail.com>
2026-02-26 16:47:32 +0000

    added code

--- app/main.py
+++ app/main.py
@@ -1,1 +1,31 @@
-# write your code here
+from __future__ import annotations
+
+
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.health = health
+        self.name = name
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def reduce_health(self, health: int) -> None:
+        self.health = max(0, self.health - health)
+
+        if self.health == 0:
+            for i in range(len(Animal.alive)):
+                if Animal.alive[i] == self:
+                    Animal.alive.pop(i)
+                    return
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, animal: Animal) -> None:
+        if animal.hidden is False and isinstance(animal, Herbivore):
+            animal.reduce_health(50)
